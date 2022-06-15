a5df922e5e21e75fedb9d817b0fca1046a25aaf4
sanshirookazaki <sanshirookazaki@gmail.com>
2022-06-15 23:52:46 +0900

    delete bash and tmux

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p ~/.cache ~/.config
 
-for dotfile in .bash_profile .bashrc .zshrc .vim .vimrc .gitcommit_template .gitignore_global .tmux.conf .config/starship.toml
+for dotfile in .zshrc .vim .vimrc .gitcommit_template .gitignore_global .config/starship.toml
 do
   if [ -e ~/$dotfile ]; then
     rm -fr ~/$dotfile
